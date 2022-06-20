e7c70658564d004884cad850b5c76c312c200743
Jeziorna Ula <ulajeziorna.de@gmail.com>
2022-06-20 14:50:17 +0200

    Logowanie

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -2,19 +2,23 @@ import "./AppNav.css"
 
 import { Link } from "react-router-dom"
 
-const AppNav = () => {
+const AppNav = (props) => {
   return (
     <nav className="mainNav">
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
+        {!props.user && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {!props.user && (
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        )}
       </ul>
     </nav>
   )
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,11 +4,14 @@ import Home from "../views/Home"
 import Login from "../views/Login"
 import Signup from "../views/Signup"
 
-const AppRoutes = () => {
+const AppRoutes = (props) => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Login />}></Route>
+      <Route
+        path="/login"
+        element={<Login user={props.user} setUser={props.setUser} />}
+      ></Route>
       <Route path="/signup" element={<Signup />}></Route>
     </Routes>
   )
